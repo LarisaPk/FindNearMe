f6092d02f3a1bc72b3d4c24f767a356d46ed2f4d
Larisa Pyykölä <Larisa.pyykola@gmail.com>
2019-04-18 21:17:19 +0300

    splash screen ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,25 @@
+// Larisa Pyykölä, student number 1702357
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+// added timer import
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+
+  showSplash = true; // <-- show animation
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
+
   ) {
     this.initializeApp();
   }
@@ -20,7 +27,8 @@ export class AppComponent {
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      this.splashScreen.hide(); // <-- hide static image
+      timer(4000).subscribe(() => this.showSplash = false); // <-- hide animation after 4s
     });
   }
 }
